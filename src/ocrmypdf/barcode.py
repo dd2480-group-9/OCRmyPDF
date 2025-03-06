e9fa1fc96afea8dc7c6dccd655fa4d6dc5e85f11
Sebkri555 <sebbe.kristoffersson@gmail.com>
2025-03-06 15:05:27 +0100

    docs(barcode.py): add/update comments and docstrings for all functions

--- src/ocrmypdf/barcode.py
+++ src/ocrmypdf/barcode.py
@@ -6,24 +6,40 @@ import os
 import random
 
 def create_pdf_with_barcode(output_filename, barcode_path, barcode_data):
+    """
+    Function that generates a pdf and adds a png
+    
+    param: 
+     output_filename : string of what user wants the filename to be
+     barcode_path : string that contains path of the barcode png 
+     barcode_data : string of barcode numbers
 
-    if not output_filename.lower().endswith(".pdf"):
+    returns: None
+
+    """
+    if not output_filename.lower().endswith(".pdf"):  # if filename does not contain end with .pdf add this here
         output_filename += ".pdf"  
-    c = canvas.Canvas(output_filename, pagesize=letter)
-    c.drawImage(f"{barcode_path}.png", 100, 500, width=400, height=200)
-    c.drawString(100, 480, f"Barcode: {barcode_data}")
-    c.save()
+    c = canvas.Canvas(output_filename, pagesize=letter)   # creates a canvas instance 
+    c.drawImage(f"{barcode_path}.png", 100, 500, width=400, height=200) # Creates image on pdf
+    c.drawString(100, 480, f"Barcode: {barcode_data}")  # Additional text alongside png on pdf
+    c.save()   # saves all edits to given path in drawImage 
 
 def generate_barcode(barcode_data):
-
-   current_directory = os.getcwd()  
-   barcode_filename = "barcode"
+   """
+   Function that generates a png of a barcode and saves it as barcode.png in current directory
+   param:
+    barcode_data : string of barcode numbers
+   return: 
+    barcode_path: string containing path of where png has been saved 
+   """
+   current_directory = os.getcwd()  # Gets current directory path
+   barcode_filename = "barcode"     # Filename of png being created 
    
-   barcode_path = os.path.join(current_directory, barcode_filename)  
+   barcode_path = os.path.join(current_directory, barcode_filename)  # joining path and name 
    
-   barcode = Code128(barcode_data, writer=ImageWriter())
+   barcode = Code128(barcode_data, writer=ImageWriter()) # Creates barcode 
    
-   barcode.save(barcode_path)
+   barcode.save(barcode_path) # Saves the barcode to path (adding .png)  
    
    return barcode_path
 
